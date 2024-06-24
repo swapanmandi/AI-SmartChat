9c1ee4583713bca8b337779857ccd8e903d614d3
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-06-24 21:57:46 +0530

    add different chat button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import LeftSidebar from '../components/LeftSidebar'
-import Prompt from '../components/Prompt'
-import Chat from '../components/Chat'
-import ChatApp from '../sample'
+import React from "react";
+import LeftSidebar from "../components/LeftSidebar";
+import Prompt from "../components/Prompt";
+import Chat from "../components/Chat";
+import ChatApp from "../sample";
 
 export default function Home() {
   return (
-   <>
-   <div className=' bg-slate-50 w-full flex justify-start'>
-   <LeftSidebar />
-   <div className=' right-0 bg-slate-800 w-[700px] h-screen'>
-   <div>Home</div>
-   
-   <Chat />
-   
-   </div> 
-   <div className=" bg-orange-300 bottom-0 fixed left-1/2">
-          
+    <>
+    <div className=" bg-slate-900 w-auto">
+      <div className=" text-white">
+        <li className=" flex p-3">
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Explain Code</ul>
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Summarizer</ul>
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Translation</ul>
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Question Answering</ul>
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Explanation</ul>
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Conversation Starting</ul>
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Data Analysis</ul>
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Solution</ul>
+          <ul className=" bg-red-300 rounded-md p-1 m-2">Chat</ul>
+          <ul className=" bg-orange-500 rounded-md p-1 m-2">New Chat</ul>
+        </li>
+        
+      </div>
+      <div className=" h-fit w-screen flex justify-start">
+        <LeftSidebar />
+        <div className=" min-h-screen  w-screen h-fit">
+          <Chat />
         </div>
+
         {/* <ChatApp /> */}
-   </div>
-   </>
-  )
+      </div>
+      </div>
+    </>
+  );
 }

03bb2d19928dbe7ddf4a9d7f843254224c8ecdea
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-06-24 22:35:06 +0530

    improve input field

--- frontend/src/components/Prompt.jsx
+++ frontend/src/components/Prompt.jsx
@@ -7,22 +7,27 @@ const [code, setCode] = useState('');
 
 const handleSubmit = (e) =>{
   e.preventDefault();
+  
+  
   const sendCode = {
     data: code
   }
   receivedCode(sendCode);
+  setCode("")
 }
   
 
 
   return (
    <>
-   <div className=' '>
-   <form onSubmit={handleSubmit} className=' bottom-0 fixed'>
+   <div className=" fixed left-1/2 bottom-0 bg-slate-900 rounded-md m-3">
+   <form onSubmit={handleSubmit}>
             <label>
-              <textarea className=' w-96 h-20 m-2' value={code} onChange={e => setCode(e.target.value)}></textarea>
+              <input className=' bg-slate-900 w-96 h-20 rounded-md' value={code} onChange={e => setCode(e.target.value)} placeholder='Write Your Query . . . . .'>
+              </input>
+              <button type='submit' disabled={code === ""} className="disabled:bg-gray-400 bg-yellow-700 p-1 rounded-md m-1">Send</button>
             </label>
-            <button type='submit'>Send</button>
+            
             </form>
    </div>
    </>

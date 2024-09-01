e3bfe33809a76fe7b59f6551f574151c7219b24c
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-09-01 22:48:06 +0530

    basic functions added

--- backend/src/models/chat.model.js
+++ backend/src/models/chat.model.js
@@ -1,22 +1,33 @@
 import mongoose from "mongoose";
 
-const chatSchema = new mongoose.Schema({
-    senderId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        require: true
+const chatSchema = new mongoose.Schema(
+  {
+    participants: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
     },
-    receiverId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        require: true
-    },
-    chat:{
-        type: String,
-        require: true
-    }
-},{
-    timestamps: true
-})
+    chats: [
+      {
+        senderId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        receiverId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        message: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Chat  = mongoose.model("Chat", chatSchema)
+export const Chat = mongoose.model("Chat", chatSchema);

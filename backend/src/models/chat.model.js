7ab096e5addeb9cd716c78802514bdd0947b3757
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-09-04 11:01:24 +0530

    update chat model for helping scoket.io easy

--- backend/src/models/chat.model.js
+++ backend/src/models/chat.model.js
@@ -1,30 +1,61 @@
 import mongoose from "mongoose";
 
 const chatSchema = new mongoose.Schema(
+
   {
-    participants: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "User",
-    },
-    chats: [
-      {
-        senderId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-          required: true,
-        },
-        receiverId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-          required: true,
-        },
-        message: {
-          type: String,
-          required: true,
-        },
-      },
-    ],
-  },
+  //   participants: {
+  //     type: [mongoose.Schema.Types.ObjectId],
+  //     ref: "User",
+  //   },
+  //   chats: [
+  //     {
+  //       senderId: {
+  //         type: mongoose.Schema.Types.ObjectId,
+  //         ref: "User",
+  //         required: true,
+  //       },
+  //       receiverId: {
+  //         type: mongoose.Schema.Types.ObjectId,
+  //         ref: "User",
+  //         required: true,
+  //       },
+  //       message: {
+  //         type: String,
+  //         required: true,
+  //       },
+  //     },
+  //   ],
+
+
+name: {
+  type: String,
+  required: function(){
+    return this.isRoomChat
+  }
+
+},
+isRoomChat:{
+  type:Boolean,
+  default : false
+
+},
+participants:{
+  type: [mongoose.Schema.Types.ObjectId],
+  ref: "User"
+}
+,
+lastMessage: {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "Message"
+},
+admin:{
+  type: mongoose.Schema.Types.ObjectId,
+  ref: "User",
+  required: function(){
+    return this.isRoomChat
+  }
+}
+   },
   {
     timestamps: true,
   }

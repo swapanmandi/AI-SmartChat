74cc347a103034bd8baa4d93dc87b8943c1090de
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-09-18 23:55:21 +0530

    update ui

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Prompt from "./Prompt.jsx";
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
@@ -9,78 +9,81 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import { useParams } from "react-router-dom";
 import AiChat from "./AiChat.jsx";
 import { io } from "socket.io-client";
-
+import { useSocket } from "../store/SocketContext.jsx";
+import { useUser } from "../store/UserContext.jsx";
+import { authContext } from "../store/AuthContextProvider.jsx";
 
 export default function Chat() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
   const [sessionId, setSessionId] = useState(uuidv4());
-  const [chatHistory, setChatHistory] = useState(null);
   const [copiedIndex, setCopiedIndex] = useState(null);
   const [sendStatus, setSendStatus] = useState(false);
   const [loading, setLoading] = useState(true);
   const [isClickedDetails, setIsClickedDetails] = useState(false);
   const [roomUserList, setRoomUserlist] = useState(null);
   const [chatUser, setChatUser] = useState(null);
-  const [user, setUser] = useState("");
   const [isClickedAiChat, setIsClickedAiChat] = useState(false);
-  const [chatId, setChatId] = useState("")
+  const [chatId, setChatId] = useState("");
+  const [loadingMessages, setLoadingMessages] = useState(true);
+  const [isConnected, setIsConnected] = useState(false);
+  const [selectMessageId, setSelectMessageId] = useState("");
+  const [viewMessageOptions, setViewMessageOptions] = useState(false);
 
-
-
-
-
+  const { getUserList } = useUser();
+  const { userId } = useContext(authContext);
   const { id, roomId } = useParams();
-  console.log("receiverId id", id, "-", roomId);
+  console.log("receiverId id", id);
   // if (id && sessionId !== id) {
   //   setSessionId(id);
   // }
 
+  console.log("chatId", chatId);
 
-  const createChat = async() =>{
-    const result = await axios.post(`http://localhost:8000/chat/oneononechat/${id}`, {},{
-      withCredentials: true
-    })
+  const socket = useSocket();
 
-    setChatId(result.data.data._id)
-    
-  }
-
-  const handleSendMessageBtn = async (e) => {
+  const createChat = async () => {
     try {
-      e.preventDefault();
-      // if (id) {
-      //   setSessionId(id);
-      // }
-      setMessages([...messages, message]);
+      const result = await axios.post(
+        `http://localhost:8000/chat/oneononechat/${id}`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      setChatId(result.data.data._id);
+    } catch (error) {
+      console.error("Error to retrive chat", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-      if (roomId) {
-        const result = await axios.post(
-          "http://localhost:8000/room/create-message",
-          { chatId: roomId, content: message },
-          {
-            withCredentials: true,
-          }
-        );
-      } else {
-        const response = await axios.post(
-          `http://localhost:8000/room/create-message/${chatId}`,
-          {content: message},
-          {
-            withCredentials: true,
-          }
-        );
-        console.log("message", response.data.data.response);
-      }
+  const onConnect = () => {
+    setIsConnected(true);
+  };
 
+  const onDisconnect = () => {
+    setIsConnected(false);
+  };
+
+
+// send message
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        `http://localhost:8000/room/create-message/${chatId}`,
+        { content: message },
+        {
+          withCredentials: true,
+        }
+      );
+
+      socket.emit("typing", chatId);
+      console.log("message", response.data.data.response);
       setSendStatus(true);
-
-      // setMessages([
-      //   ...messages,
-      //   { sender: "user", chat: message },
-      //   { sender: "model", chat: response.data.data.response },
-      // ]);
-
       setMessage("");
     } catch (error) {
       console.error("error to send message", error);
@@ -89,42 +92,86 @@ export default function Chat() {
     }
   };
 
-  //get old chat
 
-  const getChatHistory = async () => {
+  //Get Chat Messages
+  const getChatMessages = async () => {
+    if (!chatId) {
+      return alert("No chat is selected");
+    }
+
+    if (!socket) {
+      return alert("Soocket is not avialable");
+    }
+
+    socket.emit("joinChat", chatId);
+
     const response = await axios.get(
-      `http://localhost:8000/chat/chatHistory/${id}`,
+      `http://localhost:8000/room/get-messages/${chatId}`,
       {
         withCredentials: true,
       }
     );
-
-    //console.log("old chat:", response.data.data.chats);
-
-    setChatHistory(response.data.data.chats);
-    setUser(response.data.data.user);
+    console.log("getting chat messages:", response.data.data);
+    setMessages(response.data.data);
   };
 
-  const getRoomHistory = async () => {
-    const result = await axios.get(
-      `http://localhost:8000/room/get-message/${roomId}`,
+// delete message
+  const deleteMessage = async (id) => {
+    axios.delete(
+      `${import.meta.env.VITE_BACKEND_API}/room/delete-message/${chatId}/${id}`,
       { withCredentials: true }
     );
-    //console.log("room chat", result.data.data.messages);
-    setChatHistory(result.data.data.messages);
-    setUser(result.data.data.user);
+    socket.emit("messageDelete", id)
+    console.log("id", id)
+    //setMessages(prevMsg => prevMsg.filter(msg => msg._id !== id))
+    setViewMessageOptions(false)
   };
+
+// message receive event
+  const onMessageReceived = (newMessage) => {
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+  };
+
+  //message delete event
+  const onMessageDeleted = (messageId) =>{
+    setMessages(prevMsg => prevMsg.filter(msg => msg._id !== messageId._id))
+    console.log("Deleted message with ID:", messageId._id);
+  }
+ 
+
 
   useEffect(() => {
     if (id) {
-      createChat()
-      getChatHistory();
+      createChat();
     }
-    if (roomId) {
-      getRoomHistory();
+  }, [id]);
+
+  useEffect(() => {
+    getUserList();
+  }, []);
+
+  useEffect(() => {
+    setMessages(null);
+    if (!loading && chatId) {
+      getChatMessages();
     }
-    setChatHistory(null);
-  }, [id, roomId]);
+  }, [chatId]);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    socket.on("connected", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("messageReceived", onMessageReceived);
+    socket.on("messageDelete", onMessageDeleted)
+
+    return () => {
+      socket.off("connected", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("messageReceived", onMessageReceived);
+      socket.off("messageDelete", onMessageDeleted)
+    };
+  });
 
   //copy to clipboard handle
 
@@ -173,8 +220,6 @@ export default function Chat() {
     }
   };
 
-  console.log("chat length", chatHistory);
-
   const time = new Date();
   const timeFormat = (e) => {
     const date = new Date(e);
@@ -199,110 +244,126 @@ export default function Chat() {
     setIsClickedAiChat(false);
   };
 
+  const clickedOnMessage = (id) => {
+    setViewMessageOptions(!viewMessageOptions);
+    setSelectMessageId(id);
+  };
+
+
+
+  console.log("messages", messages)
+
   return (
     <>
-      <div className=" bg-slate-900 h-svh lg:w-full overflow-y-auto-auto flex flex-col">
-        <div className=" h-16 flex items-center justify-between">
-          <div
-            onClick={clickedChat}
-            className={` ${
-              isClickedAiChat ? "bg-emerald-300" : " bg-slate-900"
-            } h-full w-1/2  flex items-center justify-around`}
-          >
+      <div className=" bg-slate-900 h-screen w-full pb-10">
+        <div className=" h-[10%]">
+          <div className=" h-full flex items-center justify-between">
             <div
-              onClick={clickedDetails}
-              className=" h-10 w-10 rounded-full bg-slate-500 m-2"
-            > </div>
-            <span className="  text-white font-bold"> Chat</span>
-          </div>
-          <div
-            onClick={clickedAiChat}
-            className={` h-full w-1/2 ${
-              isClickedAiChat ? "bg-slate-900" : "bg-emerald-300"
-            }  flex items-center justify-center`}
-          >
-            <div className=" mr-2 text-white font-bold">Ai Chat Room</div>
-          </div>
-        </div>
-        {isClickedDetails && (
-          <div className=" h-80 w-60 bg-slate-700">
-            <div>
-              Account Info
-              {roomUserList?.map((item) => (
-                <div key={item._id}>{item.fullName}</div>
-              ))}
-              {chatUser && (
-                <div className=" flex flex-col">
-                  <span>Name: {chatUser?.fullName}</span>
-                  <span>Email: {chatUser.email}</span>
-                </div>
-              )}
+              onClick={clickedChat}
+              className={` ${
+                isClickedAiChat ? "bg-emerald-300" : " bg-slate-900"
+              } h-full w-1/2  flex items-center justify-around`}
+            >
+              <div
+                onClick={clickedDetails}
+                className=" h-10 w-10 rounded-full bg-slate-500 m-2"
+              >
+                {" "}
+              </div>
+              <span className="  text-white font-bold"> Chat</span>
+            </div>
+            <div
+              onClick={clickedAiChat}
+              className={` h-full w-1/2 ${
+                isClickedAiChat ? "bg-slate-900" : "bg-emerald-300"
+              }  flex items-center justify-center`}
+            >
+              <div className=" mr-2 text-white font-bold">Ai Chat Room</div>
             </div>
           </div>
-        )}
-
-        {isClickedAiChat ? (
-          <AiChat roomId={roomId} />
-        ) : (
-          <div className=" h-full w-full">
-            <div className=" h-[75%] overflow-y-auto">
-              {chatHistory ? (
-                chatHistory?.map((item) => (
-                  <div
-                    className={` w-full flex ${
-                      item.senderId?.fullName === user && "justify-end"
-                    }`}
-                  >
-                    <div className=" h-fit w-fit text-black m-2 flex justify-between">
-                      <span
-                        className={` ${
-                          item.senderId?.fullName === user && "hidden"
-                        } bg-slate-400 h-8 w-8 rounded-full p-1 m-1`}
-                      >
-                        {" "}
-                        {item.senderId?.fullName}{" "}
-                      </span>
-
-                      <span className=" bg-slate-50 rounded-md p-1 m-1">
-                        {item.message}
-                      </span>
-                    </div>
-                  </div>
-                ))
-              ) : (
-                <div>No Chats</div>
-              )}
-
-              <div className=" w-full flex justify-end">
-                {messages?.map((item) => (
-                  <div className=" bg-slate-50 h-fit w-fit text-black m-2 flex flex-col p-1 rounded-md">
-                    <span className="">{item}</span>
-                    <span className=" p-1 m-1">
-                      {timeFormat(time)} {loading && "🔄"} {sendStatus && "✔"}
-                    </span>
-                  </div>
+          {isClickedDetails && (
+            <div className=" h-80 w-60 bg-slate-700">
+              <div>
+                Account Info
+                {roomUserList?.map((item) => (
+                  <div key={item._id}>{item.fullName}</div>
                 ))}
+                {chatUser && (
+                  <div className=" flex flex-col">
+                    <span>Name: {chatUser?.fullName}</span>
+                    <span>Email: {chatUser.email}</span>
+                  </div>
+                )}
               </div>
             </div>
+          )}
+        </div>
 
-            <div className="flex justify-center items-center rounded-md lg:min-h-8 lg:w-full overflow-hidden pb-8 ">
-              <form
-                className=" bg-blue-500 rounded-sm p-1 flex justify-around items-center lg:w-1/2"
-                onSubmit={handleSendMessageBtn}
+        <div className=" bg-orange-400 h-[75%] overflow-y-scroll pb-10">
+          {messages ? (
+            messages?.map((item) => (
+              <div
+                key={item._id}
+                className={` w-full flex ${
+                  item.sender[0]?._id === userId && "justify-end"
+                }`}
               >
-                <textarea
-                  className=" outline-none line-clamp-3  m-2 p-3 w-10/12 overflow-y-auto bg-blue-500 text-black"
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  placeholder="Your Query"
-                ></textarea>
-                <button className=" bg-amber-600 h-fit w-fit p-2 rounded-md">
-                  Send
-                </button>
-              </form>
-            </div>
-          </div>
-        )}
+                {console.log(item.sender[0]?._id, userId)}
+                <div className=" h-fit w-fit text-black m-2 flex justify-between">
+                  <span
+                    className={` ${
+                      item.sender[0]?._id === userId && "hidden"
+                    } bg-slate-400 h-8 w-8 rounded-full p-1 m-1`}
+                  >
+                    {item.sender[0]?.fullName}
+                  </span>
+                  {selectMessageId === item._id && viewMessageOptions && (
+                    <div className=" space-x-2">
+                      <span>Share</span>
+                      <span onClick={() => deleteMessage(item._id)} className=" cursor-pointer">
+                        Delete
+                      </span>
+                    </div>
+                  )}
+                  <span
+                    onClick={() => clickedOnMessage(item._id)}
+                    className=" bg-slate-50 rounded-md p-1 m-1 cursor-pointers"
+                  >
+                    {item.content}
+                  </span>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div>No Chats</div>
+          )}
+        </div>
+
+        <div className=" h-[15%] flex justify-center items-center rounded-md ">
+          <form
+            className=" bg-blue-500 h-full rounded-sm p-1 flex justify-center items-center  lg:w-1/2"
+            onSubmit={sendMessage}
+          >
+            <input
+              className=" outline-none line-clamp-3  m-2 p-3 w-10/12 overflow-y-auto bg-blue-500 text-black"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Your Query"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault()
+                  sendMessage();
+                }
+              }}
+            ></input>
+            <button
+              className=" bg-amber-600 h-fit w-fit p-2 rounded-md disabled:bg-gray-300"
+              disabled={!message}
+            >
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -1,16 +1,20 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useUser } from "../store/UserContext.jsx";
+import { useSocket } from "../store/SocketContext.jsx";
+import { authContext } from "../store/AuthContextProvider.jsx";
 
 export default function LeftSidebar() {
   const [isClickedCreateBtn, setIsClickedCreateBtn] = useState(false);
   const [isClickedCreateRoom, setisClickedCreateRoom] = useState(false);
   const [selectUser, setSelectUser] = useState([]);
-  const [roomName, setRoomName] = useState("")
+  const [roomName, setRoomName] = useState("");
+  const [chats, setChats] = useState(null);
+  const [oneOnOneChatUsers, setOneOnOneChatUsers] = useState(null)
+  const { setIsDeleted, userList, getUserList } = useUser();
 
-  const { setIsDeleted, userList,  rooms } = useUser();
-
+  const { userId } = useContext(authContext);
   //delete chat
 
   const handleChatDelete = async (item) => {
@@ -35,32 +39,53 @@ export default function LeftSidebar() {
     setisClickedCreateRoom(!isClickedCreateRoom);
   };
 
-
-  const handleRoomNameChange = (e) =>{
-setRoomName(e.target.value)
-  }
+  const handleRoomNameChange = (e) => {
+    setRoomName(e.target.value);
+  };
 
   const handleInputChange = (e) => {
     const val = e.target.value;
     const isChecked = e.target.checked;
     if (isChecked) {
       setSelectUser([...selectUser, val]);
-    }else{
-      console.log(val)
-      setSelectUser(selectUser.filter(item => item !== val))
+    } else {
+      console.log(val);
+      setSelectUser(selectUser.filter((item) => item !== val));
     }
   };
 
   const handleCreateRoom = async (e) => {
     e.preventDefault();
     await axios.post(
-      "http://localhost:8000/room/create-room",
-     {roomName, users: selectUser},
+      "http://localhost:8000/chat/create-roomchat",
+      { name: roomName, participants: selectUser },
       {
         withCredentials: true,
       }
     );
   };
+
+  useEffect(() => {
+    const chats = async () => {
+      const result = await axios.get(
+        `${import.meta.env.VITE_BACKEND_API}/chat/chats`,
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("chat list", result.data.data);
+      setChats(result.data.data);
+    };
+
+    chats();
+  }, []);
+
+
+const messageRef = useRef()
+
+
+
+
 
   return (
     <>
@@ -77,7 +102,7 @@ setRoomName(e.target.value)
                 onClick={clickedCreateRoom}
                 className=" bg-orange-400 p-1 m-2"
               >
-                Create AI Chat Room
+                Create Chat Room
               </button>
               <button className=" bg-orange-400 p-1 m-2">Add Friend</button>
             </div>
@@ -86,7 +111,12 @@ setRoomName(e.target.value)
               <div>
                 <form onSubmit={handleCreateRoom}>
                   <h2>Room Name:</h2>
-                  <input value={roomName} onChange={handleRoomNameChange} type="text" className=" outline-none p-1 m-2 text-black"></input>
+                  <input
+                    value={roomName}
+                    onChange={handleRoomNameChange}
+                    type="text"
+                    className=" outline-none p-1 m-2 text-black"
+                  ></input>
                   <ul>
                     {userList?.map((item) => (
                       <li className=" m-1 p-1 bg-slate-600 flex justify-between px-3">
@@ -107,39 +137,38 @@ setRoomName(e.target.value)
           </div>
         )}
         <div className=" overflow-y-auto">
-          {userList?.map((item, index) => (
+          {chats?.map((item, index) => (
             <div key={item._id} className=" m-1 overflow-y-auto flex">
-              <Link to={`/app/chat/${item._id}`}>
-                {" "}
-                <h2 className="  rounded-md hover:bg-slate-500 p-2" key={index}>
-                  {item.fullName}
-                </h2>
-              </Link>
+              <h2 className="  rounded-md hover:bg-slate-500 p-2" key={index}>
+                {item.isRoomChat ? (
+                  <span>
+                    <Link to={`/app/chat/${item._id}`}>{item.name}</Link>
+                  </span>
+                ) : (
+                  item.participants.map(
+                    (user) =>
+                      user._id !== userId && (
+                       <>
+                     
+                        <span s ref={messageRef} className=" cursor-pointer">
+                          <Link to={`/app/chat/${user._id}`}>
+                            {user.fullName}
+                          </Link>
+                        </span>
+
+                        </>
+                      )
+                  )
+                )}
+              </h2>
+
               <button
                 onClick={() => handleChatDelete(item._id)}
                 className=" px-2 mx-3"
-              >
-                
-              </button>
+              ></button>
             </div>
           ))}
 
-{rooms?.map((item, index) => (
-            <div key={item._id} className=" m-1 overflow-y-auto flex">
-              <Link to={`/app/room-chat/${item._id}`}>
-                
-                <h2 className="  rounded-md hover:bg-slate-500 p-2" key={index}>
-                  {item.name}
-                </h2>
-              </Link>
-              <button
-                onClick={() => handleChatDelete(item._id)}
-                className=" px-2 mx-3"
-              >
-                
-              </button>
-            </div>
-          ))}
         </div>
       </div>
     </>

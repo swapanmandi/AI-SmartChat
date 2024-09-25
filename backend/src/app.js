fb891f8656b868ede4dae5d160bd4cbfeb248374
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-09-25 10:21:27 +0530

    update route declarations

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,31 +1,54 @@
-import express from 'express'
-import cors from 'cors'
-import chatRouter from './routes/chat.route.js'
-import signupRouter from './routes/signupRoute.js'
-
+import express from "express";
+import cors from "cors";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import { initializeSocketIO } from "./utils/socket.js";
+import chatRouter from "./routes/chat.route.js";
+import userRouter from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
+import roomRouter from "./routes/room.routes.js";
+import messageRouter from "./routes/message.routes.js";
+import aiRoomMessageRouter from "./routes/aiRoomChat.routes.js";
 
 const app = express();
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials: true
-}));
-app.use(express.json({limit: '16kb'}))
-app.use(express.urlencoded({
-    extended:true,
-    limit: '16kb'
-}))
+const server = createServer(app);
 
-// app.use((req, res, next) =>{
-//     console.log('Request Method:', req.method);
-//     console.log('Request Headers:', req.headers);
-//     console.log('Request Body:', req.body);
-//     next();
-// })
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  },
+});
 
+app.set("io", io);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-//route ddeclaration
-app.use('/', chatRouter)
-app.use('/app/user/', signupRouter)
+initializeSocketIO(io);
 
-export {app}
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+app.use(cookieParser());
+
+//route declarations
+app.use("api/v1/chat-app/users", userRouter);
+app.use("api/v1//chat-app/chats", chatRouter);
+app.use("api/v1/chat-app/messages", messageRouter);
+app.use("/room", roomRouter, aiRoomMessageRouter);
+
+export { server };

c8850ba7980529be6770ff09e4af189242074b4e
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-09-27 20:54:36 +0530

    create ai chat and get chats

--- backend/src/controllers/aiChat.controller.js
+++ backend/src/controllers/aiChat.controller.js
@@ -8,16 +8,15 @@ const createAiChat = asyncHandler(async (req, res) => {
     const { id: chatId } = req.params;
     const {participants} = req.body
 
-    const existedAiChat = await AiChat.findOne({ chat: chatId });
-    let aiChat;
+    let existedAiChat = await AiChat.findOne({ chat: chatId });
 
     if (!existedAiChat) {
-      aiChat = await AiChat.create({ chat: chatId, participants });
+      existedAiChat = await AiChat.create({ chat: chatId, participants });
     }
 
     return res
       .status(200)
-      .json(new ApiResponse(200, aiChat, "Ai Chat created successfully"));
+      .json(new ApiResponse(200, existedAiChat, "Ai Chat created successfully"));
   } catch (error) {
     throw new ApiError(500, "Error to create ai chat- internal server error");
   }
--- backend/src/controllers/aiMessage.controller.js
+++ backend/src/controllers/aiMessage.controller.js
@@ -5,6 +5,35 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { AiMessage } from "../models/aiMessage.model.js";
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import mongoose from "mongoose";
+
+const commonAiMessageAggregation = () => {
+  return [
+    {
+      $lookup: {
+        from: "aichats",
+        foreignField: "_id",
+        localField: "aiChat",
+        as: "aiChat",
+        pipeline: [
+          {
+            $project: {
+              chat: 1,
+              participants: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        aiChat: {
+          $first: "$aiChat",
+        },
+      },
+    },
+  ];
+};
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
@@ -12,7 +41,7 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 const createAiChat = asyncHandler(async (req, res) => {
   try {
     const { id: aiChatId } = req.params;
-    const {query } = req.body;
+    const { query } = req.body;
 
     if (query === "") {
       throw new ApiError(400, "The field is empty");
@@ -32,15 +61,15 @@ const createAiChat = asyncHandler(async (req, res) => {
     //result = await chat.sendMessage("How many paws are in my house?");
     const text = result.response.text();
 
-    let existedAiChat = await AiChat.findOne({ _id: aiChatId });
+    let existedAiChat = await AiChat.findById(aiChatId);
     console.log("existed chat", existedAiChat);
 
     //const sender = req.user?._id;
 
     //if (existedAiChat) {
-      // existedAiChat.messages.push({ sender, query });
-      // existedAiChat.messages.push({ sender: null, query: text });
-      // await existedAiChat.save();
+    // existedAiChat.messages.push({ sender, query });
+    // existedAiChat.messages.push({ sender: null, query: text });
+    // await existedAiChat.save();
     //   await AiMessage.create({})
     // } else {
     //   existedAiChat = await AiChat.create({
@@ -52,39 +81,56 @@ const createAiChat = asyncHandler(async (req, res) => {
     //   });
     // }
 
-    if(!existedAiChat){
-      throw new ApiError(404, "Chat does not exis")
+    if (!existedAiChat) {
+      throw new ApiError(404, "Chat does not exis");
     }
 
-    await AiMessage.create({aiChat: aiChatId, sender:{role: "user", user: req.user?._id}, content: query})
-    await AiMessage.create({aiChat: aiChatId, sender:{role: "model", user: null}, content: text})
-    
+    await AiMessage.create({
+      aiChat: aiChatId,
+      sender: { role: "user", user: req.user?._id },
+      content: query,
+    });
+    await AiMessage.create({
+      aiChat: aiChatId,
+      sender: { role: "model", user: null },
+      content: text,
+    });
+
     return res
       .status(200)
-      .json(new ApiResponse(200, {text}, "ai chat send successfully"));
+      .json(new ApiResponse(200, { text }, "ai chat send successfully"));
   } catch (error) {
     throw new ApiError(404, "There is an error while create ai chat", error);
   }
 });
 
-const getAiRoomChat = asyncHandler(async (req, res) => {
+const getAiMessages = asyncHandler(async (req, res) => {
   const { id: aiChatId } = req.params;
 
-  const chats = await AiChat.findOne({ aiChatId }).populate('messages.sender', 'fullName');
+  const aiChat = await AiChat.findById(aiChatId);
 
-  if (!chats) {
-    throw new ApiError(404, "No Chats");
+  if (!aiChat) {
+    throw new ApiError(404, "Chat does not exist");
   }
+
+  const messages = await AiMessage.aggregate([
+    {
+      $match: {
+        aiChat: new mongoose.Types.ObjectId(aiChatId),
+      },
+    },
+    ...commonAiMessageAggregation(),
+  ]);
 
   return res
     .status(200)
     .json(
       new ApiResponse(
         200,
-        {chats: chats.messages, user: req.user?.fullName},
-        "Ai Room Chats fetched Successfully."
+       messages,
+        "Ai Chats fetched Successfully."
       )
     );
 });
 
-export { createAiChat, getAiRoomChat };
+export { createAiChat, getAiMessages };

82b5c17d55039901b5d1a478ace0f4a4df3f1bde
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-09-28 21:25:48 +0530

    update it

--- backend/src/controllers/aiMessage.controller.js
+++ backend/src/controllers/aiMessage.controller.js
@@ -39,21 +39,43 @@ const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const createAiChat = asyncHandler(async (req, res) => {
-  try {
+  
     const { id: aiChatId } = req.params;
     const { query } = req.body;
+    //console.log("query:", query)
 
     if (query === "") {
-      throw new ApiError(400, "The field is empty");
+      throw new ApiError(400, "The query field is empty!");
     }
 
-    const chatHistory = await AiMessage.findOne({ aiChat: aiChatId });
+    let existedAiChat = await AiChat.findById(aiChatId);
+    console.log("existed chat", existedAiChat);
+
+    if (!existedAiChat) {
+      throw new ApiError(404, "Chat does not exis");
+    }
+
+
+    if(!existedAiChat.participants.includes(req.user?._id)){
+      throw new ApiError(409, "You are not in this chat")
+    }
+
+   //await AiMessage.findOne({ aiChat: aiChatId });
+   const chatHistory = await AiMessage.aggregate([
+      {
+        $match: {
+          aiChat: new mongoose.Types.ObjectId(aiChatId),
+        },
+      },
+      ...commonAiMessageAggregation(),
+    ]);
+    console.log("CHAT HISTORY:", chatHistory)
 
     const chat = model.startChat({
       history:
         chatHistory?.map((item) => ({
           role: item.sender?.role ? "user" : "model",
-          parts: [{ text: item.query }],
+          parts: [{ text: item.content }],
         })) || [],
     });
     let result = await chat.sendMessage(query);
@@ -61,30 +83,7 @@ const createAiChat = asyncHandler(async (req, res) => {
     //result = await chat.sendMessage("How many paws are in my house?");
     const text = result.response.text();
 
-    let existedAiChat = await AiChat.findById(aiChatId);
-    console.log("existed chat", existedAiChat);
-
-    //const sender = req.user?._id;
-
-    //if (existedAiChat) {
-    // existedAiChat.messages.push({ sender, query });
-    // existedAiChat.messages.push({ sender: null, query: text });
-    // await existedAiChat.save();
-    //   await AiMessage.create({})
-    // } else {
-    //   existedAiChat = await AiChat.create({
-    //     aiChatId,
-    //     messages: [
-    //       { sender, query },
-    //       { sender: null, query: text },
-    //     ],
-    //   });
-    // }
-
-    if (!existedAiChat) {
-      throw new ApiError(404, "Chat does not exis");
-    }
-
+   
     await AiMessage.create({
       aiChat: aiChatId,
       sender: { role: "user", user: req.user?._id },
@@ -99,11 +98,10 @@ const createAiChat = asyncHandler(async (req, res) => {
     return res
       .status(200)
       .json(new ApiResponse(200, { text }, "ai chat send successfully"));
-  } catch (error) {
-    throw new ApiError(404, "There is an error while create ai chat", error);
-  }
+ 
 });
 
+// GET AI CHAT MESSAGES
 const getAiMessages = asyncHandler(async (req, res) => {
   const { id: aiChatId } = req.params;
 
@@ -134,3 +132,22 @@ const getAiMessages = asyncHandler(async (req, res) => {
 });
 
 export { createAiChat, getAiMessages };
+
+
+
+//const sender = req.user?._id;
+
+    //if (existedAiChat) {
+    // existedAiChat.messages.push({ sender, query });
+    // existedAiChat.messages.push({ sender: null, query: text });
+    // await existedAiChat.save();
+    //   await AiMessage.create({})
+    // } else {
+    //   existedAiChat = await AiChat.create({
+    //     aiChatId,
+    //     messages: [
+    //       { sender, query },
+    //       { sender: null, query: text },
+    //     ],
+    //   });
+    // }

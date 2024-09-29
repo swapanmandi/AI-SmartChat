cc709071505665638b56798fc1c02b768e7cd1b4
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-09-29 14:55:10 +0530

    update auth checking

--- backend/src/controllers/aiChat.controller.js
+++ backend/src/controllers/aiChat.controller.js
@@ -2,16 +2,26 @@ import { AiChat } from "../models/aiChat.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
+import {Chat} from "../models/chat.model.js"
 
 const createAiChat = asyncHandler(async (req, res) => {
   try {
     const { id: chatId } = req.params;
-    const {participants} = req.body
+   
+    const chat = await Chat.findById(chatId)
+
+    if(!chat){
+      throw new ApiError(404, "Chat does not exist")
+    }
+
+    if(!chat.participants?.includes(req.user?._id)){
+      throw new ApiError(409, "You are not in this chat")
+    }
 
     let existedAiChat = await AiChat.findOne({ chat: chatId });
 
     if (!existedAiChat) {
-      existedAiChat = await AiChat.create({ chat: chatId, participants });
+      existedAiChat = await AiChat.create({ chat: chatId, participants: chat.participants });
     }
 
     return res

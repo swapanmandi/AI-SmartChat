5b10c9b74f33630046c8a026a024551a9d10d0f8
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-10-06 08:57:42 +0530

    add Auth0 authentication

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,30 +1,85 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
+import Cookies from "js-cookie";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext, useAuth } from "../store/AuthContext.jsx";
+import {useAuth0 } from "@auth0/auth0-react";
 
-export default function () {
+
+
+export default function Header() {
+  const navigate = useNavigate();
+  const [isClickProfile, setIsClickProfile] = useState(false);
+
+
+
+  const {user,isAuthenticated, logout } = useAuth0();
+  const { signout, token } = useAuth();
+  //console.log("token", token)
+console.log("auth0 user",user)
+  const clickProfile = () => {
+    console.log("clicked");
+    setIsClickProfile((prev) => !prev);
+  };
+
+  const handleLogout = async () => {
+    try {
+      if(isAuthenticated){
+        logout({ logoutParams: { returnTo: navigate("/signin") } })
+      }
+      signout();
+      // Cookies.remove("accessToken");
+      // Cookies.remove("refreshToken");
+    } catch (error) {
+      console.error("Logout failed:", error.message);
+    }
+  };
+
   return (
-   <>
-    <div>
-    <div className=" text-white">
-          <li className=" lg:flex p-3">
-            <ul className=" bg-red-300 rounded-md p-1 m-2">Explain Code</ul>
-            <ul className=" bg-red-300 rounded-md p-1 m-2">Summarizer</ul>
-            <ul className=" bg-red-300 rounded-md p-1 m-2">Translation</ul>
-            <ul className=" bg-red-300 rounded-md p-1 m-2">
-              Question Answering
-            </ul>
-            <ul className=" bg-red-300 rounded-md p-1 m-2">Explanation</ul>
-            <ul className=" bg-red-300 rounded-md p-1 m-2">
-              Conversation Starting
-            </ul>
-            <ul className=" bg-red-300 rounded-md p-1 m-2">Data Analysis</ul>
-            <ul className=" bg-red-300 rounded-md p-1 m-2">Solution</ul>
-            <ul className=" bg-red-300 rounded-md p-1 m-2">Chat</ul>
-            <ul className=" bg-orange-500 rounded-md p-1 m-2">New Chat</ul>
-          </li>
-        </div>
-    </div>
-   
-   </>
-  )
+    <div className=" bg-teal-800 w-12 flex flex-col items-center p-1">
+      
 
+      {token && isAuthenticated && (
+        <div className=" flex flex-col space-y-4">
+          <Link to="/app/profile"> <button className=""><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" /><path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" /></svg></button></Link>
+         
+
+          <button className={`${isClickProfile && " bg-slate-600"}`} onClick={clickProfile}>
+          <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="currentColor"  class="icon icon-tabler icons-tabler-filled icon-tabler-settings"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14.647 4.081a.724 .724 0 0 0 1.08 .448c2.439 -1.485 5.23 1.305 3.745 3.744a.724 .724 0 0 0 .447 1.08c2.775 .673 2.775 4.62 0 5.294a.724 .724 0 0 0 -.448 1.08c1.485 2.439 -1.305 5.23 -3.744 3.745a.724 .724 0 0 0 -1.08 .447c-.673 2.775 -4.62 2.775 -5.294 0a.724 .724 0 0 0 -1.08 -.448c-2.439 1.485 -5.23 -1.305 -3.745 -3.744a.724 .724 0 0 0 -.447 -1.08c-2.775 -.673 -2.775 -4.62 0 -5.294a.724 .724 0 0 0 .448 -1.08c-1.485 -2.439 1.305 -5.23 3.744 -3.745a.722 .722 0 0 0 1.08 -.447c.673 -2.775 4.62 -2.775 5.294 0zm-2.647 4.919a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" /></svg>
+          </button>
+
+          <div
+            className={`${
+              isClickProfile ? "block" : "hidden"
+            }`}
+          >
+            <ul className=" absolute z-30 bg-slate-600 left-12 flex flex-col">
+              <li className="p-2 font-semibold border-b">
+                View Profile
+              </li>
+              <li className="p-2 font-semibold border-b">
+                <Link to="/settings">Settings</Link>
+              </li>
+              <li
+                className="p-2 font-semibold cursor-pointer"
+                onClick={handleLogout}
+              >
+                Logout
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+
+      {!token && !isAuthenticated && (
+        <div className="flex space-x-4">
+          <button className="flex items-end justify-end m-3">
+            <Link to="/signup">Sign Up</Link>
+          </button>
+          <button>
+            <Link to="/signin">Sign In</Link>
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,91 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./index.css";
+import PublicHome from "./pages/PublicHome.jsx";
+import Home from "./pages/Home.jsx";
+import App from "./App.jsx";
+//import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import { AuthProvider } from "./store/AuthContext.jsx";
+import Profile from "./components/Profile.jsx";
+import Input from "./components/Input.jsx";
+import { Auth0Provider } from '@auth0/auth0-react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const Signup = lazy(() => import("./pages/Signup.jsx"))
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <PublicHome />,
+      },
+      {
+        path: "/signup",
+        element: (<Suspense fallback={<div>Loading</div>}><Signup /></Suspense>)
+      },
+      {
+        path: "/signin",
+        element: <Login />,
+      },
+    ],
+  },
+  
+  {
+   path: "/app",
+   element: <App />,
+
+    children: [
+      {
+      path: "/app",
+    element: (
+      
+        <Home />
+      
+    )},
+    {
+
+      path:"/app/chat/:cid/:rid",
+      element:
+      <Home />
+    
+    },
+    {
+      path: "/app/room-chat/:cid",
+      element: <Home />
+    },{
+path: "/app/chat/:cid/:rid/:cq",
+element: <Home />
+    },
+      {
+        path: "/app/profile",
+        element: <Profile />
+      },
+    ],
+  },
+  {
+    path:"/test",
+    element: <Input/>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+    domain="dev-phsh8i6jitay3sct.us.auth0.com"
+    clientId="faTJHPrkSXkeekYYMZMk7LJuhA6uIKni"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+      <RouterProvider router={router}>
+      <AuthProvider />
+        </RouterProvider>
+        </Auth0Provider>
+  </React.StrictMode>
+);

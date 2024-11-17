21af230f7c9d7c708d37a9df54a838f314d2d6d3
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-11-17 12:45:01 +0530

    update option

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -91,7 +91,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "None",
     //path: '/',
     maxAge: 7200000,
   };

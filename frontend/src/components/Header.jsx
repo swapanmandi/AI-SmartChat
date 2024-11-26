92b5917dd77b7adf50451a481630c9fa5d6a8c1a
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-11-26 21:37:51 +0530

    update with custom hook and redux

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,26 @@
-import React, { useContext, useState } from "react";
-import Cookies from "js-cookie";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { AuthContext, useAuth } from "../store/AuthContext.jsx";
+import { useAuth } from "../store/AuthContext.jsx";
+import Settings from "./Settings.jsx";
+import Profile from "./Profile.jsx";
 
 export default function Header({ setClickedMobChat, clickedMobChat }) {
-  const navigate = useNavigate();
   const [isClickProfile, setIsClickProfile] = useState(false);
+  const [isClickedSettings, setIsClickedSettings] = useState(false);
   const [showHeader, setShowHeader] = useState(false);
+  const [isClickedAccount, setIsClickedAccount] = useState(false);
+
   const { signout, token } = useAuth();
   //console.log("token", token)
 
   const clickProfile = () => {
-    console.log("clicked");
+    //console.log("clicked");
     setIsClickProfile((prev) => !prev);
   };
 
   const handleLogout = async () => {
     try {
       signout();
-      // Cookies.remove("accessToken");
-      // Cookies.remove("refreshToken");
     } catch (error) {
       console.error("Logout failed:", error.message);
     }
@@ -37,31 +38,29 @@ export default function Header({ setClickedMobChat, clickedMobChat }) {
     <div className=" w-full  h-full items-center flex flex-col justify-center">
       <div
         className={` bg-teal-800 lg:h-full w-full lg:w-12 lg:flex flex-col items-center p-1 ${
-          showHeader ? "flex" : "hidden"
+          showHeader ? "block" : "hidden"
         }`}
       >
         {token && (
-          <div className=" w-full items-center flex justify-between flex-col space-y-4">
-            <Link to="/app/profile">
-              <button className="">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="icon icon-tabler icons-tabler-filled icon-tabler-user"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
-                  <path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" />
-                </svg>
-              </button>
-            </Link>
+          <div className=" w-full h-full items-center flex justify-between flex-row lg:flex-col  p-1">
+            <button className=" profile " onClick={() => clickProfile()}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="icon icon-tabler icons-tabler-filled icon-tabler-user"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 2a5 5 0 1 1 -5 5l.005 -.217a5 5 0 0 1 4.995 -4.783z" />
+                <path d="M14 14a5 5 0 0 1 5 5v1a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-1a5 5 0 0 1 5 -5h4z" />
+              </svg>
+            </button>
 
             <button
-              className={`${isClickProfile && " bg-slate-600"}`}
-              onClick={clickProfile}
+              className=" settings "
+              onClick={() => setIsClickedSettings(!isClickedSettings)}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -75,21 +74,41 @@ export default function Header({ setClickedMobChat, clickedMobChat }) {
                 <path d="M14.647 4.081a.724 .724 0 0 0 1.08 .448c2.439 -1.485 5.23 1.305 3.745 3.744a.724 .724 0 0 0 .447 1.08c2.775 .673 2.775 4.62 0 5.294a.724 .724 0 0 0 -.448 1.08c1.485 2.439 -1.305 5.23 -3.744 3.745a.724 .724 0 0 0 -1.08 .447c-.673 2.775 -4.62 2.775 -5.294 0a.724 .724 0 0 0 -1.08 -.448c-2.439 1.485 -5.23 -1.305 -3.745 -3.744a.724 .724 0 0 0 -.447 -1.08c-2.775 -.673 -2.775 -4.62 0 -5.294a.724 .724 0 0 0 .448 -1.08c-1.485 -2.439 1.305 -5.23 3.744 -3.745a.722 .722 0 0 0 1.08 -.447c.673 -2.775 4.62 -2.775 5.294 0zm-2.647 4.919a3 3 0 1 0 0 6a3 3 0 0 0 0 -6z" />
               </svg>
             </button>
+            {isClickedSettings && (
+              <div
+                className={` absolute lg:left-12 bg-zinc-100 lg:w-60 lg:h-full `}
+              >
+                <ul className=" z-30 bg-slate-600  flex flex-col">
+                  <li
+                    onClick={() => setIsClickedAccount(!isClickedAccount)}
+                    className="p-2 font-semibold border-b"
+                  >
+                    Accounts
+                  </li>
 
-            <div className={`${isClickProfile ? "flex" : "hidden"}`}>
-              <ul className=" absolute z-30 bg-slate-600 lg:left-12 flex flex-col">
-                <li className="p-2 font-semibold border-b">View Profile</li>
-                <li className="p-2 font-semibold border-b">
-                  <Link to="/settings">Settings</Link>
-                </li>
-                <li
-                  className="p-2 font-semibold cursor-pointer"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </li>
-              </ul>
-            </div>
+                  <li className="p-2 font-semibold border-b">General</li>
+                  <li className="p-2 font-semibold border-b">Chats</li>
+                  <li
+                    className="p-2 font-semibold cursor-pointer"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </li>
+
+                  {isClickedAccount && (
+                    <div className=" absolute lg:left-60 lg:h-60 lg:w-60 bg-green-400">
+                      <Settings />
+                    </div>
+                  )}
+                </ul>
+              </div>
+            )}
+
+            {isClickProfile && (
+              <div className=" absolute left-12">
+                <Profile />
+              </div>
+            )}
           </div>
         )}
 
@@ -104,7 +123,7 @@ export default function Header({ setClickedMobChat, clickedMobChat }) {
           </div>
         )}
       </div>
-      <div className=" flex space-x-72 justify-between">
+      <div className=" flex space-x-72 justify-between lg:hidden">
         {clickedMobChat && (
           <div onClick={handleClickedBackToChat}>
             <svg

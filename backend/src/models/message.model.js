84bf5ecd1f0efc119ce06365140ab124e2cc61c6
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-11-26 22:04:40 +0530

    basic update

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,46 +1,32 @@
 import mongoose from "mongoose";
 
-const messageModel = new mongoose.Schema({
-  // roomId: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "Room",
-  // },
-  // messages: [
-  //   {
-  //     senderId: {
-  //       type: mongoose.Schema.Types.ObjectId,
-  //       ref: "User",
-  //       required: true,
-  //     },
-   
-  //     message: {
-  //       type: String,
-  //       required: true,
-  //     },
-  //   },
-  // ],
-
-
-  sender:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+const messageModel = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: function () {
+        return this.attachments.length === 0;
+      },
+    },
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+    attachments: {
+      type: [
+        {
+          url: String,
+          localPath: String,
+        },
+      ],
+      default: [],
+    },
   },
-  content: {
-    type: String,
-    required: true
-  },
-  chat: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Chat"
-  },
-  attachments: {
-    type: [{
-      url: String,
-      localPath: String
-    }],
-    default : []
-  }
-
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 export const Message = mongoose.model("Message", messageModel);

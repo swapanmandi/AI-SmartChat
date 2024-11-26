82685da9107e47760220d2f1782a529be13e91ee
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-11-26 21:41:59 +0530

    update

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import { AuthContext } from "../store/AuthContext.jsx";
 import { useContext } from "react";
 
@@ -15,23 +14,23 @@ export default function Profile() {
   //console.log("user details", userDetails)
 
   return (
-    <>
-      <div className=" z-0 bg-slate-900 text-white absolute h-svh w-svw flex justify-center items-center">
-        <div className=" bg-slate-600 h-5/6 justify-center p-4 flex rounded-md w-4/12">
-          <div>
-            Profile
-            {userDetails ? (
-              <div className="">
-                
-                <p>FullName:{userDetails.data?.fullName || userDetails.fullName} </p>
-                <p>Email:{userDetails.data?.email || userDetails.email} </p>
-              </div>
-            ) : (
-              "There is no User"
-            )}
-          </div>
+    <div className="absolute h-svh w-svw flex justify-center items-center">
+      <div className=" bg-slate-600 h-5/6 justify-center p-4 flex rounded-md w-4/12">
+        <div>
+          Profile
+          {userDetails ? (
+            <div className="">
+              <img src={userDetails?.data?.avatar}></img>
+              <p>
+                FullName:{userDetails.data?.fullName || userDetails.fullName}{" "}
+              </p>
+              <p>Email:{userDetails.data?.email || userDetails.email} </p>
+            </div>
+          ) : (
+            "There is no User"
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 }

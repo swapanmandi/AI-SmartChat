af6b363a26b50387f2b70eea5c10b49471f9de53
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-11-30 13:24:18 +0530

    remove header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,21 @@
-import React, { useState } from "react";
-import Chat from "../pages/Chat.jsx";
-import Header from "../components/Header.jsx";
+import React from "react";
+import Header from "../components/Header";
+import LeftSidebar from "../components/LeftSidebar";
 
 export default function Home() {
-  const [clickedMobChat, setClickedMobChat] = useState(false);
-
-  //console.log("c m status", clickedMobChat)
   return (
-    <>
-      <div className=" h-svh fixed w-screen flex flex-col lg:flex-row">
-        <div>
-          <Header
-            setClickedMobChat={setClickedMobChat}
-            clickedMobChat={clickedMobChat}
-          />
-        </div>
+    <div className=" flex h-full w-full flex-col lg:flex-row ">
 
-        <div>
-          <Chat
-            clickedMobChat={clickedMobChat}
-            setClickedMobChat={setClickedMobChat}
-          />
+      <div className=" w-full lg:w-96">
+        <LeftSidebar />
+      </div>
+
+      <div className=" bg-red-500 h-full w-full flex items-center justify-center">
+        <div className=" text-center">
+        <h2>Select a Chat and Start Messeging with</h2>
+        <h1>SMART AI CHAT APP</h1>
         </div>
       </div>
-    </>
+    </div>
   );
 }

aa76db8270412939e87ebacfbb8ca3850a4af334
SWAPAN MANDI <swapanmandi5@gmail.com>
2024-11-30 13:21:34 +0530

    update

--- frontend/src/store/AuthContext.jsx
+++ frontend/src/store/AuthContext.jsx
@@ -5,6 +5,14 @@ import Loader from "../components/Loader.jsx";
 
 const AuthContext = createContext();
 
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
 const AuthProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [token, setToken] = useState(null);
@@ -12,22 +20,41 @@ const AuthProvider = ({ children }) => {
 
   const navigate = useNavigate();
 
+  //console.log("auth token", token);
 
   const signup = async (fullName, email, password) => {
-    const data = { fullName, email, password };
-    await axios.post(`${import.meta.env.VITE_BACKEND_API}/users/signup`, data, {
-      withCredentials: true,
-    });
-    navigate("/login");
+    setIsLoading(true);
+    try {
+      const data = { fullName, email, password };
+      await axios.post(
+        `${import.meta.env.VITE_BACKEND_API}/users/signup`,
+        data,
+        {
+          withCredentials: true,
+        }
+      );
+      navigate("/login");
+    } catch (error) {
+      console.error("Error to sign up.", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const signin = async (email, password) => {
-    const data = { email, password };
-    const result = await axios.post(
-      `${import.meta.env.VITE_BACKEND_API}/users/signin`,
-      data,
-      { withCredentials: true }
-    );
+    setIsLoading(true);
+    try {
+      const data = { email, password };
+      const result = await axios.post(
+        `${import.meta.env.VITE_BACKEND_API}/users/signin`,
+        data,
+        { withCredentials: true }
+      );
+    } catch (error) {
+      console.error("Error to sign in", error);
+    } finally {
+      setIsLoading(false);
+    }
 
     navigate("/app");
     setToken(result.data.data.accessToken);
@@ -37,37 +64,40 @@ const AuthProvider = ({ children }) => {
     localStorage.setItem("user", JSON.stringify(result.data.data.user));
   };
 
-
-
   const signout = async () => {
-    await axios.post(
-      `${import.meta.env.VITE_BACKEND_API}/users/signout`,
-      {},
-      {
-        withCredentials: true,
-      }
-    );
-    navigate("/signin");
-    setToken(null);
-    setUser(null);
-    localStorage.clear();
+    setIsLoading(true);
+
+    try {
+      await axios.post(
+        `${import.meta.env.VITE_BACKEND_API}/users/signout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      navigate("/signin");
+      setToken(null);
+      setUser(null);
+      localStorage.clear();
+    } catch (error) {
+      console.error("Error to sign out.", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
-   
-      setIsLoading(true);
-      const _token = localStorage.getItem("token");
-      const _user = JSON.parse(localStorage.getItem("user"));
+    setIsLoading(true);
+    const _token = localStorage.getItem("token");
+    const _user = JSON.parse(localStorage.getItem("user"));
 
-      if (_token && _user) {
-        setToken(_token);
-        setUser(_user);
-      } 
-      
-      setIsLoading(false);
+    if (_token && _user) {
+      setToken(_token);
+      setUser(_user);
+    }
 
-  }, [navigate]);
-
+    setIsLoading(false);
+  }, []);
 
   return (
     <AuthContext.Provider
@@ -77,15 +107,12 @@ const AuthProvider = ({ children }) => {
         signout,
         token,
         user,
+        isLoading,
       }}
     >
       {isLoading ? <Loader /> : children}
     </AuthContext.Provider>
   );
-};
-
-const useAuth = () => {
-  return useContext(AuthContext);
 };
 
 export { AuthContext, AuthProvider, useAuth };
